cfd15cfd88a69b25c91eef01e505a79d828fde9e
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-06-14 15:18:42 +0200

    dynamic product catalogue

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@ let logo = document.querySelector('.logo')
 
 window.addEventListener('scroll', () => {
     const scrolled = window.scrollY
-    if (scrolled > 700) {
+    if(scrolled > 700) {
         navbar.style.backgroundColor = 'black'
         navbar.style.color = 'white'
         logo.setAttribute('src', 'public/img/white-logo.png')
@@ -43,10 +43,32 @@ for (let index = 0; index < buttoncarroussel.length; index++) {
 
 //section7 product gallery
 let productsi = document.getElementsByClassName("products")[0]
+let imagesproducts = document.getElementsByClassName('imgproduct')
 
 //row1 - the row-cols-5 in the bootstrap defines the wrap
 for (let i = 0; i < 10; i++) {
+    //creation of elements
     let div = document.createElement("div")
+    let div3 = document.createElement('div')
+    let div4 = document.createElement('div')
+    let iconheart = document.createElement("i")
+    let iconleft = document.createElement("i")
+    let iconright = document.createElement("i")
+
+    let heartnav = document.getElementsByTagName("i")[2]
+    //position and custom font-awesome classes 
+    iconleft.classList.add("fa-solid", "fa-layer-group")
+    iconright.classList.add("fa-solid", "fa-eye")
+    iconheart.classList.add("fa-solid", "fa-heart")
+    div.style.position = "relative"
+    iconheart.style.position = "absolute"
+    iconleft.style.position = "absolute"
+    iconright.style.position = "absolute"
+    //visibility
+    iconheart.style.visibility = "hidden"
+    iconleft.style.visibility = "hidden"
+    iconright.style.visibility = "hidden"
+    //col for bootstrap and assign content into it
     div.classList.add("col")
     let img = document.createElement('img')
     img.setAttribute('src', productstable[i].image)
@@ -55,6 +77,7 @@ for (let i = 0; i < 10; i++) {
     name.innerHTML = productstable[i].name
     name.style.fontWeight = "bold"
     let oldprice = document.createElement('p')
+    //div that contains the prices for display flex
     let divprices = document.createElement('div')
     oldprice.innerHTML = productstable[i].oldprice
     oldprice.style.textDecoration = "line-through"
@@ -64,32 +87,70 @@ for (let i = 0; i < 10; i++) {
     newprice.innerHTML = productstable[i].newprice
     newprice.style.color = "rgb(177, 171, 171)"
     newprice.style.padding = "10px"
-    divprices.appendChild(oldprice)
-    divprices.appendChild(newprice)
     divprices.style.display = "flex"
     divprices.style.justifyContent = "center"
+    div3.style.backgroundColor = "white"
+    div3.style.opacity = "0.9"
+    div3.style.height ="50px"
+    div3.style.width = "50%"
+    div4.style.backgroundColor = "white"
+    div4.style.height ="50px"
+    div4.style.width = "50%"
+    div3.style.position = "absolute"
+    div4.style.position = "absolute"
+    div4.style.right = "0"
+    div4.style.bottom = "25%"
+    div3.style.left = "0"
+    div3.style.bottom = "25%"
+    //append child
+    productsi.appendChild(div)
+    divprices.appendChild(oldprice)
+    divprices.appendChild(newprice)
     div.appendChild(img)
     div.appendChild(name)
     div.appendChild(divprices)
-    productsi.appendChild(div)
-}
+    div.appendChild(iconheart)
+   
+    //icon left and right positioning
 
-//onhover image changes and the icons appears
+    iconleft.style.top = "45%"
+    iconright.style.top = "45%"
 
-let imagesproducts = document.getElementsByClassName('imgproduct')
-
-for (let i = 0; i < imagesproducts.length; i++) {
+    //onhover image changes and the icons appears
     imagesproducts[i].addEventListener("mouseover", () => {
         imagesproducts[i].setAttribute('src',  productstable[i].image2)
+        iconheart.style.visibility = "visible"
+        iconleft.style.visibility = "visible"
+        iconright.style.visibility = "visible"
+        iconheart.style.right ="10%"
+        iconheart.style.top ="5%"
+        div3.appendChild(iconleft)
+        div4.appendChild(iconright)
+        div.appendChild(div3)
+        div.appendChild(div4)
         setTimeout(() =>{
             imagesproducts[i].setAttribute('src',  productstable[i].image)
+            iconheart.style.visibility = "hidden"
+            iconleft.style.visibility = "hidden"
+            iconright.style.visibility = "hidden"
+            div3.removeChild(iconleft)
+            div4.removeChild(iconright)
+            div.removeChild(div3)
+            div.removeChild(div4)
         },2000)
+        let numbernav = 0
+        heartnav.innerHTML = numbernav
+        iconheart.addEventListener('click', () =>{
+            numbernav += 1
+        })
     })
 }
 
+//carrousel testimonial
 
 //darkmode
 
-//carrousel testimonial
+
+
 
 //decount products
