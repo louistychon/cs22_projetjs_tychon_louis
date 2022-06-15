1b1e240917954d9b2c130a3c3fa88b49fa261277
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-06-15 14:16:48 +0200

    modifications carroussel js

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,16 @@
 import {
     productstable
 } from './products.js'
-console.log(productstable)
+
+import {
+    testimonialstable
+} from './testimonials.js'
 
 //header change color regarding scroll position
 let navbar = document.querySelector('nav')
 let navlinks = document.querySelectorAll('.nav-link')
 let logo = document.querySelector('.logo')
+let burgermenu = document.querySelector('.fa-bars')
 
 window.addEventListener('scroll', () => {
     const scrolled = window.scrollY
@@ -14,12 +18,14 @@ window.addEventListener('scroll', () => {
         navbar.style.backgroundColor = 'black'
         navbar.style.color = 'white'
         logo.setAttribute('src', 'public/img/white-logo.png')
+        burgermenu.style.color = 'white'
         navlinks.forEach(element => {
             element.classList.add('navbar-links-white')
         });
     } else {
         navbar.style.backgroundColor = '#f5ebdf'
         navbar.style.color = 'black'
+        burgermenu.style.color = 'black'
         logo.setAttribute('src', 'public/img/logo.png')
         navlinks.forEach(element => {
             element.classList.remove('navbar-links-white')
@@ -28,16 +34,21 @@ window.addEventListener('scroll', () => {
 })
 
 
-//carroussel home
+//carroussel section 1
 let section1 = document.getElementById("section1")
 let imgcarroussel = ["public/img/collect/one-page-img-04.jpg", "public/img/collect/one-page-slider-01.jpg", "public/img/collect/one-page-img-04.jpg", "public/img/collect/one-page-slider-01.jpg"]
 let buttoncarroussel = document.getElementsByClassName('button-carroussel')
+
 
 for (let index = 0; index < buttoncarroussel.length; index++) {
     buttoncarroussel[index].addEventListener("click", () => {
         let img = imgcarroussel[index]
         section1.style.backgroundImage = "url(" + img + ")"
     })
+    setTimeout(() =>{ 
+         let img2 = imgcarroussel[2]
+        section1.style.backgroundImage = "url(" + img2 + ")"
+    }, 2000)
 }
 
 
@@ -55,8 +66,8 @@ for (let i = 0; i < 10; i++) {
     let iconheart = document.createElement("i")
     let iconleft = document.createElement("i")
     let iconright = document.createElement("i")
-    let heartnav = document.getElementsByTagName("i")[4]
-    let basketnav = document.getElementsByTagName("i")[5]
+    let heartnav = document.getElementsByTagName("i")[3]
+    let basketnav = document.getElementsByTagName("i")[4]
     //position and custom font-awesome classes 
     iconleft.classList.add("fa-solid", "fa-layer-group")
     iconright.classList.add("fa-solid", "fa-eye")
@@ -140,22 +151,22 @@ for (let i = 0; i < 10; i++) {
         }, 2000)
     })
     //increase number of likes in the nav
-    let numberlikes = 0
-    let productsliked = []
     iconheart.addEventListener('click', () => {
         iconheart.style.color = "red"
         if (iconheart.style.color == "red") {
-            productsliked.push(productstable[i].name)
-        } else {
-
+            heartnav.innerHTML = +heartnav.innerHTML + 1
         }
-        heartnav.innerHTML += productsliked.length
     })
     //increase number of products in basket
-    let numberbasket = 0
     iconleft.addEventListener('click', () => {
-        basketnav.innerHTML = numberbasket += 1
+        iconleft.style.color = "grey"
+        if (iconheart.style.color == "grey") {
+            basketnav.innerHTML = +basketnav.innerHTML + 1
+        } else {
+            basketnav.innerHTML = +basketnav.innerHTML + 1
+        }
     })
+    //iconeye onclick event
     iconright.addEventListener('click', () => {
         img.style.transform = "scale(1.5)";
         img.style.transition = "transform 0.25s ease";
@@ -179,14 +190,110 @@ for (let i = 0; i < 10; i++) {
 
 //carrousel testimonial
 
-let leftarrow = document.getElementsByClassName('fa-arrow-left')[0]
-let rightarrow = document.getElementsByClassName('fa-arrow-right')[0]
-let testimonials = document.getElementsByClassName('testimonial')
+let containerCarroussel = document.getElementsByClassName("carroussel-testimonials")[0]
+
+let tablecolumns = [];
+let rightarrow = document.createElement('i')
+rightarrow.classList.add('fa-solid', 'fa-arrow-right')
+let leftarrow = document.createElement('i')
+leftarrow.classList.add('fa-solid', 'fa-arrow-left')
+let col1 = document.createElement('div')
+col1.classList.add('col-1')
+let col12 = document.createElement('div')
+col12.classList.add('col-1')
+col1.appendChild(leftarrow)
+col12.appendChild(rightarrow)
+
+containerCarroussel.appendChild(col1)
+
+for (let i = 0; i < 6; i++) {
+    let desc = document.createElement('p')
+    let title = document.createElement('p')
+    let column = document.createElement('div')
+    column.classList.add('element-carroussel')
+    let whitebackground = document.createElement('div')
+    let divavatar = document.createElement('div')
+    divavatar.classList.add('avatar')
+    let row2 = document.createElement('div')
+    row2.classList.add('row')
+    let col4 = document.createElement('div')
+    col4.classList.add('col-4', "text-center", "mt-2")
+    let imgavatar = document.createElement('img')
+    imgavatar.setAttribute('src', testimonialstable[i].image)
+    imgavatar.classList.add('rounded-circle', "img-fluid", "w-50")
+    whitebackground.style.backgroundColor = "white"
+    whitebackground.classList.add('whitebg')
+    let pname = document.createElement('p')
+    let pjob = document.createElement('p')
+    pname.innerHTML = testimonialstable[i].name
+    pjob.innerHTML = testimonialstable[i].job
+    column.appendChild(whitebackground)
+    column.classList.add('col')
+    tablecolumns.push(column)
+    title.innerHTML = testimonialstable[i].title
+    desc.innerHTML = testimonialstable[i].desc
+    whitebackground.appendChild(title)
+    whitebackground.appendChild(desc)
+    column.appendChild(divavatar)
+    divavatar.appendChild(row2)
+    let columnavatar = document.createElement('div')
+    let columnavatar2 = document.createElement('div')
+    columnavatar.classList.add("col-4")
+    columnavatar2.classList.add("col-8")
+    row2.appendChild(columnavatar)
+    columnavatar.appendChild(imgavatar)
+    columnavatar2.appendChild(pname)
+    columnavatar2.appendChild(pjob)
+    row2.appendChild(columnavatar2)
+    pname.style.fontWeight = "bold"
+    containerCarroussel.appendChild(column)
+}
+
+let columns = document.getElementsByClassName('element-carroussel')
+
+columns[0].style.display = ""
+columns[1].style.display = ""
+columns[2].style.display = ""
+columns[3].style.display = "none"
+columns[4].style.display = "none"
+columns[5].style.display = "none"
 
 leftarrow.addEventListener('click', () => {
-    testimonials[0].style.transform = "" 
-    testimonials.appendChild(testimonials[0])
+    if (columns[0].style.display == "") {
+        columns[0].style.display = "none"
+        columns[1].style.display = "none"
+        columns[2].style.display = "none"
+        columns[3].style.display = ""
+        columns[4].style.display = ""
+        columns[5].style.display = ""
+    } else {
+        columns[0].style.display = ""
+        columns[1].style.display = ""
+        columns[2].style.display = ""
+        columns[3].style.display = "none"
+        columns[4].style.display = "none"
+        columns[5].style.display = "none"
+    }
 })
+rightarrow.addEventListener('click', () => {
+    if (columns[0].style.display == "") {
+        columns[0].style.display = "none"
+        columns[1].style.display = "none"
+        columns[2].style.display = "none"
+        columns[3].style.display = ""
+        columns[4].style.display = ""
+        columns[5].style.display = ""
+    } else {
+        columns[0].style.display = ""
+        columns[1].style.display = ""
+        columns[2].style.display = ""
+        columns[3].style.display = "none"
+        columns[4].style.display = "none"
+        columns[5].style.display = "none"
+    }
+})
+
+containerCarroussel.appendChild(col12)
 
 
 //darkmode
@@ -208,8 +315,7 @@ moon.addEventListener('click', () => {
         for (let index = 0; index < para.length; index++) {
             para[index].style.color = "white"
         }
-    }
-    else{
+    } else {
         document.body.style.backgroundColor = "white"
         for (let i = 0; i < card.length; i++) {
             card[i].style.backgroundColor = "white"
